4d9f5882d6cdd554e4c7585ad3ed2832bd5180ec
Andrey <ushakov.andrey.34@gmail.com>
2020-03-11 13:49:39 +0100

    PosCodeTable: added new predefined code tables

--- lib/src/enums.dart
+++ lib/src/enums.dart
@@ -54,16 +54,16 @@ class PosCodeTable {
   final int value;
 
   /// PC437 - U.S.A., Standard Europe
-  static const pc437 = PosCodeTable(0);
+  static const pc437_1 = PosCodeTable(0);
 
   /// Katakana
-  static const katakana = PosCodeTable(1);
+  static const katakana1 = PosCodeTable(1);
 
   /// PC850 Multilingual
   static const pc850 = PosCodeTable(2);
 
   /// PC860 - Portuguese
-  static const pc860 = PosCodeTable(3);
+  static const pc860_1 = PosCodeTable(3);
 
   /// PC863 - Canadian-French
   static const pc863 = PosCodeTable(4);
@@ -77,6 +77,93 @@ class PosCodeTable {
   /// Greek
   static const greek = PosCodeTable(7);
 
+  /// Hebrew
+  static const hebrew1 = PosCodeTable(8);
+
+  /// East Europe
+  static const eastEur = PosCodeTable(9);
+
+  /// Iran
+  static const iran1 = PosCodeTable(10);
+
+  /// WPC1252
+  static const wpc1252_1 = PosCodeTable(16);
+
+  /// PC866 - Cyrillic #2
+  static const pc866_2 = PosCodeTable(17);
+
+  /// PC852 - Latin2
+  static const pc852_1 = PosCodeTable(18);
+
+  /// PC858
+  static const pc858_1 = PosCodeTable(19);
+
+  /// IranII
+  static const iran2 = PosCodeTable(20);
+
+  /// Latvian
+  static const latvian = PosCodeTable(21);
+
+  /// Arabic
+  static const arabic = PosCodeTable(22);
+
+  /// PT1511251
+  static const pt1511251 = PosCodeTable(23);
+
+  /// PC747
+  static const pc747 = PosCodeTable(24);
+
+  /// WPC1257
+  static const wpc1257 = PosCodeTable(25);
+
+  /// Vietnam
+  static const vietnam = PosCodeTable(27);
+
+  /// PC864
+  static const pc864_1 = PosCodeTable(28);
+
+  /// PC1001
+  static const pc1001_1 = PosCodeTable(29);
+
+  /// Uigur
+  static const uigur = PosCodeTable(30);
+
+  /// Hebrew
+  static const hebrew2 = PosCodeTable(31);
+
+  /// WPC1255 - Israel
+  static const israel = PosCodeTable(32);
+
+  /// PC437 - Std.Europe
+  static const pc437_2 = PosCodeTable(50);
+
+  /// Katakana
+  static const katakana2 = PosCodeTable(51);
+
+  /// PC437 - Std.Europe
+  static const pc437_3 = PosCodeTable(52);
+
+  /// PC858 - Multilingual
+  static const pc858_2 = PosCodeTable(53);
+
+  /// PC852 - Latin-2
+  static const pc852_2 = PosCodeTable(54);
+
+  /// PC860 - Portuguese
+  static const pc860_2 = PosCodeTable(55);
+
+  /// PC866 - Russian
+  static const pc866_1 = PosCodeTable(59);
+
+  /// PC857 - Turkey
+  static const pc857 = PosCodeTable(61);
+
+  /// PC862 - Hebrew
+  static const pc862 = PosCodeTable(62);
+
+  /// PC864 - Arabic
+  static const pc864_2 = PosCodeTable(63);
+
   /// PC737 - Greek
   static const pc737 = PosCodeTable(64);
 
@@ -89,20 +176,32 @@ class PosCodeTable {
   /// PC928 - Greek
   static const pc928 = PosCodeTable(67);
 
-  /// PC866 - Russian
-  static const pc866_1 = PosCodeTable(59);
+  /// PC874 - Thai
+  static const pc874 = PosCodeTable(70);
 
-  /// PC866 - Cyrillic #2
-  static const pc866_2 = PosCodeTable(17);
-
-  /// PC852 - Latin2
-  static const pc852 = PosCodeTable(18);
+  /// WPC1252 - Latin1
+  static const wpc1252_2 = PosCodeTable(71);
 
   /// WPC1251 - Cyrillic
   static const wpc1251 = PosCodeTable(73);
 
-  /// WPC1252 - Latin1
-  static const wpc1252 = PosCodeTable(71);
+  /// IBM-Russian
+  static const pc3840 = PosCodeTable(74);
+
+  /// PC3843 - Polish
+  static const pc3843 = PosCodeTable(76);
+
+  /// PC3846 - Turkish
+  static const pc3846 = PosCodeTable(79);
+
+  /// PC1001 - Arabic
+  static const pc1001_2 = PosCodeTable(82);
+
+  /// PC3011 - Latvian-1
+  static const pc3011 = PosCodeTable(86);
+
+  /// PC3012 - Latvian-2
+  static const pc3012 = PosCodeTable(87);
 
   /// Space page
   static const spacePage = PosCodeTable(255);
