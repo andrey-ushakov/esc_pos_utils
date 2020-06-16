3f3e116c2ef74f56b71a511d695f4f2d5a616e7e
Andrey <ushakov.andrey.34@gmail.com>
2020-06-16 19:33:50 +0200

    update json capabilities format

--- lib/src/capability_profile.dart
+++ lib/src/capability_profile.dart
@@ -10,6 +10,12 @@ import 'dart:convert' show json;
 import 'package:resource/resource.dart' show Resource;
 import 'dart:convert' show utf8;
 
+class CodePage {
+  CodePage(this.id, this.name);
+  int id;
+  String name;
+}
+
 class CapabilityProfile {
   CapabilityProfile._internal(this.name, this.codePages);
 
@@ -26,23 +32,28 @@ class CapabilityProfile {
       throw Exception("The CapabilityProfile '$name' does not exist");
     }
 
+    List<CodePage> list = [];
+    profile['codePages'].forEach((k, v) {
+      list.add(CodePage(int.parse(k), v));
+    });
+
     // Call the private constructor
-    return CapabilityProfile._internal(
-        name, Map<String, int>.from(profile['codePages']));
+    return CapabilityProfile._internal(name, list);
   }
 
   String name;
-  Map<String, int> codePages;
+  List<CodePage> codePages;
 
   int getCodePageId(String codePage) {
     if (codePages == null) {
       throw Exception("The CapabilityProfile isn't initialized");
     }
-    int id = codePages[codePage];
-    if (id == null) {
-      throw Exception("Code Page '$codePage' isn't defined for this profile");
-    }
-    return id;
+
+    return codePages
+        .firstWhere((cp) => cp.name == codePage,
+            orElse: () => throw Exception(
+                "Code Page '$codePage' isn't defined for this profile"))
+        .id;
   }
 
   static Future<List<dynamic>> getAvailableProfiles() async {
